8365cb52a887b5d47d3b55e1f9ac5fca54e869e0
polynomial-c <87381648+mmckitrick@users.noreply.github.com>
2022-03-10 18:41:23 -0500

    constructors written

--- src/Date.java
+++ src/Date.java
@@ -2,7 +2,32 @@
 /**
  *
  * @author Macie McKitrick
+ * Program Assignment #3: Dates
+ * 3/8/22
+ * Description: Represent a date including its month, day, and year
+ * Help from: Ben Alfaro (suggestion to use constructor chaining)
  */
 public class Date {
+    private int day;
+    private int month;
+    private int year;
+    Date()
+    {
+        this(1, 1, 2000);
+    }
+    Date(int day)
+    {
+        this(day, 1, 2000);
+    }
+    Date(int day, int month)
+    {
+        this(day, month, 2000);
+    }
+    Date(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
     
 }
--- src/DateDriver.java
+++ src/DateDriver.java
@@ -2,6 +2,9 @@
 /**
  *
  * @author Macie McKitrick
+ * Program Assignment #3: Dates
+ * 3/8/22
+ * Description: test Date class constructors and methods
  */
 public class DateDriver {
     public static void main(String[] args) {
