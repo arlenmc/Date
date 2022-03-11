7533a501f2d76c26379f3707a62b3aed16dae944
polynomial-c <87381648+mmckitrick@users.noreply.github.com>
2022-03-11 10:59:46 -0500

    Date class before enums mixed in

--- src/Date.java
+++ src/Date.java
@@ -6,11 +6,27 @@
  * 3/8/22
  * Description: Represent a date including its month, day, and year
  * Help from: Ben Alfaro (suggestion to use constructor chaining)
+ * 
+ * -if time: implements enums
  */
 public class Date {
     private int day;
     private int month;
     private int year;
+    //experimental- these are properties of a date so should be determined with it-
+    //helps prevent creating nonexistent date/keeping things straight
+    // PREVENT CREATING DATE THAT DOES NOT EXIST- this is handled by isValid method
+    // buuut! class params I created will help
+    
+    // months with 31 days: January, March, May, July, August, October, December
+                            // 1,3,5,7,8,10,12
+    // months with 30 days: September, April, June, and November
+                            // 9, 4, 6, 11
+    // February varies: 28 when not leap year; 29 when leap year
+                            // 2
+    // use enum to manage month name, order, and days in month
+    private int daysInMonth;
+    private boolean leapYear;
     Date()
     {
         this(1, 1, 2000);
@@ -28,6 +44,16 @@ public class Date {
         this.day = day;
         this.month = month;
         this.year = year;
+        this.leapYear = this.year % 4 == 0;
+        if (this.month)
     }
-    
+    // advance date by one day
+    public void advance()
+    {
+        this.day++;
+    }
+    public String toString()
+    {
+        
+    }
 }
